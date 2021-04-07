01fd8452c360d6c65cd3f8418a1e62318b0d57e3
Miguel Escalante <skalas@Felurian.localdomain>
2021-04-07 13:46:08 -0500

    eshell not used for now

--- init.el
+++ init.el
@@ -190,34 +190,34 @@
   (setq vterm-shell "zsh")                       ;; Set this to customize the shell to launch
   (setq vterm-max-scrollback 10000))
 
-(defun skls/configure-eshell ()
-  ;; Save command history when commands are entered
-  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+;; (defun skls/configure-eshell ()
+;;   ;; Save command history when commands are entered
+;;   (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
 
-  ;; Truncate buffer for performance
-  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+;;   ;; Truncate buffer for performance
+;;   (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
 
-  ;; Bind some useful keys for evil-mode
-  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
-  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
-  (evil-normalize-keymaps)
+;;   ;; Bind some useful keys for evil-mode
+;;   (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
+;;   (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
+;;   (evil-normalize-keymaps)
 
-  (setq eshell-history-size         10000
-        eshell-buffer-maximum-lines 10000
-        eshell-hist-ignoredups t
-        eshell-scroll-to-bottom-on-input t))
+;;   (setq eshell-history-size         10000
+;;         eshell-buffer-maximum-lines 10000
+;;         eshell-hist-ignoredups t
+;;         eshell-scroll-to-bottom-on-input t))
 
-(use-package eshell-git-prompt
-  :after eshell)
+;; (use-package eshell-git-prompt
+;;   :after eshell)
 
-(use-package eshell
-  :hook (eshell-first-time-mode . skls/configure-eshell)
-  :config
-  (with-eval-after-load 'esh-opt
-    (setq eshell-destroy-buffer-when-process-dies t)
-    (setq eshell-visual-commands '("htop" "zsh" "vim")))
+;; (use-package eshell
+;;   :hook (eshell-first-time-mode . skls/configure-eshell)
+;;   :config
+;;   (with-eval-after-load 'esh-opt
+;;     (setq eshell-destroy-buffer-when-process-dies t)
+;;     (setq eshell-visual-commands '("htop" "zsh" "vim")))
 
-  (eshell-git-prompt-use-theme 'powerline))
+;;   (eshell-git-prompt-use-theme 'powerline))
 
 (defun skls/org-mode-babel ()
   (require 'org-tempo)
